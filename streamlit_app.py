cd1fe0e4b94367e520339688b820da5fa61bf5e8
Agustin_yalo <juan.pollio@yalo.com>
2023-04-22 16:13:56 -0300

    streamlit app mod
    rename config file
    promts file uploaded
    add figures and text to the main app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,13 @@
 from pathlib import Path
-import shutil
-
 import streamlit as st
 from langchain.llms import OpenAI
 import pandas as pd
 from dotenv import load_dotenv
-
+from PIL import Image
 from src.pandas_dataframe_agent import create_pandas_dataframe_agent_with_tools
 from src.orders import get_order_items_from_csv
+import os
+
 
 # Get the base directory
 basepath = Path()
@@ -19,17 +19,24 @@ container.title('Analytics GPT')
 
 def ask_agent(question):
     # df = get_order_items(bot_id, storefront_name=storefront_name, start_date="2023-01-01", end_date="2023-03-07")
+    try:
+        os.remove("plot.png")
+    except:
+        pass
+    
     df = get_order_items_from_csv("mockup_data_region.csv")
 
     tools = []
     llm = OpenAI(temperature=0)
     agent = create_pandas_dataframe_agent_with_tools(tools, llm, df, verbose=True, return_intermediate_steps=True)
-    response = agent({"input": question})
     
-    container.write(userPrompt)
-    container.write(response.get("output"))
-    shutil.copyfile('top_10_products_by_region.png', 'static/plot.png')
-    container.markdown("[![Click me](app/static/plot.png)](https://yalo.com)")
+    list_plot = ['graph', 'chart', 'plot']
+    if any(word in question.lower() for word in list_plot):
+        question = question + ". Save as plot.png and don't cut the labels of the plot."
+    else:
+        question = question
+    response = agent({"input": question})
+    return response
 
 
 with st.sidebar:
@@ -39,4 +46,13 @@ with st.sidebar:
     result = st.button(label='Click for Magic')
 
     if result:
-        ask_agent(userPrompt)
+        response= ask_agent(userPrompt)
+
+        with container:
+            #container.write(userPrompt)
+            try:
+                image = Image.open('plot.png')
+                st.image(image, caption='plot')
+                #os.remove("plot.png")
+            except:
+                st.header(response.get("output"))
